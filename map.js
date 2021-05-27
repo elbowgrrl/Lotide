38bfc94a05dde1ed283f750ed2be052149a3aaea
elbowgrrl <76022369+elbowgrrl@users.noreply.github.com>
2021-05-27 16:29:23 -0700

    adding map and take until

---
+++ map.js
@@ -0,0 +1,12 @@
+const words = ["flower", "dance", "absorb", "drink", "clap", "jump"]
+// a function which works like .map
+const map = (array, cb) => {
+  const mappedArray = [];
+  for (const index of array) {
+    mappedArray.push(cb(index))
+  }
+  return mappedArray;
+}
+
+console.log(map(words, word => { return `${word}ing`}))
+
---
+++ takeUntil.js
@@ -0,0 +1,26 @@
+const words = ["flower", "dance", "absorb", "drink", "clap", "jump"]
+const numbers = [3, 5, 7, 1, 9, 11, 13];
+
+//impliment a function which iterates over an array and pushes items into a
+//new array until the callbacl function returns a truthy value
+
+const takeUntil = function (array, cb) {
+  const newArray = [];
+  //iterate over array
+  for (let i = 0; i < array.length; i++) {
+    //call callback on each element
+    if (!cb(array[i])) {
+      //if callback does not evaluate to true, push element into new array
+      newArray.push(array[i]);
+    }
+    //if cb evaluates to true, return the Array
+    if (cb(array[i])) {
+      return newArray;
+    }
+  }
+  return newArray;
+};
+
+console.log(takeUntil(numbers, (x) => x < 2));
+console.log(takeUntil(words, word => word === "absorb"));
+
