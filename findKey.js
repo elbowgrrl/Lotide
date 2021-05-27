8adab46a38d5d9ed6a06f7c2ae1d45984034ad77
elbowgrrl <76022369+elbowgrrl@users.noreply.github.com>
2021-05-27 16:46:31 -0700

    adding findKey

---
+++ findKey.js
@@ -0,0 +1,25 @@
+//a function that takes in an object and a callback and returns the
+// first key for which the callback returns a truthy value
+
+const findKey = function(object, cb) {
+  for (const key in object) {
+    // console.log(object[key])
+    if (cb(object[key])) {
+      console.log(key);
+      return;
+    }
+  }
+};
+
+
+//TEST CODE
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
